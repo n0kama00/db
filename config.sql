7d95a94bb80a6b9d9abe2dbd888d941883b11fa7
Emilia Kaihua <n0kama00@students.oamk.fi>
2022-01-05 12:25:05 +0200

    files upload

---
+++ config.sql
@@ -0,0 +1,22 @@
+drop database if exists n0kama00;
+CREATE database n0kama00;
+use n0kama00;
+
+CREATE TABLE IF NOT EXISTS user (
+        first_name varchar(50) NOT NULL,
+        last_name varchar(50) NOT NULL,
+        username varchar(50) NOT NULL,
+        password varchar(150) NOT NULL,
+        PRIMARY KEY (username)
+        );
+
+CREATE TABLE IF NOT EXISTS user_info(
+    username varchar(50) NOT NULL,
+    email varchar(50) PRIMARY KEY,
+    phone int NOT NULL,
+    address varchar (50) NOT NULL,
+    zipcode varchar (5) NOT NULL,
+    city varchar(20) NOT NULL,
+    FOREIGN KEY (username) 
+    REFERENCES user(username)
+    );
---
+++ functions.php
@@ -0,0 +1,98 @@
+<?php
+
+function createDbConnection(){
+
+    try{
+        $dbcon = new PDO('mysql:host=localhost:3307;dbname=n0kama00', 'root', '');
+        $dbcon->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        createTable($dbcon);
+    }catch(PDOException $e){
+        echo '<br>'.$e->getMessage();
+    }
+
+    return $dbcon;
+}
+
+function checkUser(PDO $dbcon, $username, $passwd){
+
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+    $passwd = filter_var($passwd, FILTER_SANITIZE_STRING);
+
+    try{
+        $sql = "SELECT password FROM user WHERE username=?";  //komento, arvot parametreina
+        $prepare = $dbcon->prepare($sql);   //valmistellaan
+        $prepare->execute(array($username));  //kysely tietokantaan
+
+        $rows = $prepare->fetchAll(); //haetaan tulokset
+
+        //Käydään rivit läpi
+        foreach($rows as $row){
+            $pw = $row["password"];  //password sarakkeen tieto (hash salasana tietokannassa)
+            if( password_verify($passwd, $pw) ){  //tarkistetaan salasana tietokannan hashia vasten
+                return true;
+            }
+        }
+
+        return false;
+
+    }catch(PDOException $e){
+        echo '<br>'.$e->getMessage();
+    }
+}
+
+/**
+ * Luo tietokantaan uuden käyttäjän ja hashaa salasanan
+ */
+function createUser(PDO $dbcon, $fname, $lname, $username, $passwd){
+
+    //Sanitoidaan. Lisätty tuntien jälkeen.
+    $fname = filter_var($fname, FILTER_SANITIZE_STRING);
+    $lname = filter_var($lname, FILTER_SANITIZE_STRING);
+    $username = filter_var($username, FILTER_SANITIZE_STRING);
+    $passwd = filter_var($passwd, FILTER_SANITIZE_STRING);
+
+    try{
+        $hash_pw = password_hash($passwd, PASSWORD_DEFAULT); //salasanan hash
+        $sql = "INSERT IGNORE INTO user VALUES (?,?,?,?)"; //komento, arvot parametreina
+        $prepare = $dbcon->prepare($sql); //valmistellaan
+        $prepare->execute(array($fname, $lname, $username, $hash_pw));  //parametrit tietokantaan
+    }catch(PDOException $e){
+        echo '<br>'.$e->getMessage();
+    }
+}
+
+function createTable(PDO $con){
+    $sql = "CREATE TABLE IF NOT EXISTS user(
+        first_name varchar(50) NOT NULL,
+        last_name varchar(50) NOT NULL,
+        username varchar(50) NOT NULL,
+        password varchar(150) NOT NULL,
+        PRIMARY KEY (username)
+        )";
+
+    try{   
+        $con->exec($sql);  
+    }catch(PDOException $e){
+        echo '<br>'.$e->getMessage();
+    }
+
+    createUser($con,'Emilia','Kaihua', 'emmu', 'emmu123');
+    createUser($con,'Elina','Kaihua', 'ellu', 'ellu123');
+    createUser($con,'Oskari','Kaihua', 'osku', 'osku123');
+
+    $sql2 = "CREATE TABLE IF NOT EXISTS user_info(
+        username varchar(50) NOT NULL,
+        email varchar(50) PRIMARY KEY,
+        phone int NOT NULL,
+        address varchar (50) NOT NULL,
+        zipcode varchar (5) NOT NULL,
+        city varchar(20) NOT NULL,
+        FOREIGN KEY (username) 
+        REFERENCES user(username)
+        )";
+
+    createUserInfo($con, 'emmu', 'emmu@email.com', '0401234567', 'Oulu 1', '90100', 'Oulu' );
+
+}
+
+?>
